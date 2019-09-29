1ab115728d085758418b2f836186dba2ca240683
Andi Angel <andi.angel@boheemia.ee>
2019-09-29 10:37:58 +0300

    AJAX fixed, added language menu with english support. Cleanup of many styles, especially important change of H to P with appropriate class names

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
-﻿"use strict";
-const preloader = document.querySelector(".preloader");
-const titlePage = document.querySelector(".title");
+﻿//DOM items:
+const preloader = document.querySelector(".js-preloader");
 const container = document.querySelectorAll(".container");
-const farBack = document.querySelector(".farBack");
-const downButton = document.querySelector(".nuppAlla");
-const up = document.querySelector(".tagasiÜlesse");
-let enabled = true;
-let paused = true;
-
 const pages = document.getElementsByTagName("section");
-let read;
-read = 0;
+const appWindow = document.querySelector(".js-appWindow");
 
 
+let read = 0;
+
+//Changeable states:
+let enabled = true; //scroll
+let paused = true; //to avoid navigation js spam
+let language = "EST";
+
+//XHR state flags:
+let videoLoaded = false;
+let archiveDocLoaded = false; //KASUTAMATA
+
+const pageCount = container.length + 1;
+
+let isContentLoaded = new Array(pageCount);
+let conteinerCount = read - 1;
+
+
+//Launch initial code - removes preloader and displays first "Section":
 document.addEventListener("load", pageLoaded());
 
 function pageLoaded() {
-    setTimeout(pageStart, 2500);
-}
-
-function pageStart() {
-    preloader.classList.replace("visible", "longInvisible");
-    farBack.classList.replace("invisible", "longvisible");
-    container[0].classList.replace("invisible", "visible");
-    pages[0].classList.replace("above", "center");
-    setTimeout(hidePreloader, 3000);
-}
-
-function hidePreloader() {
-    container[0].classList.remove("delay");
-    preloader.style.display = "none";
+    resetContentStates();
+    for (read = 1; read < 6;) {
+        console.log(read);
+        askForContent();
+        console.log("preloaded index " + read);
+        read += 1;
+    }
+    read = 0;
+    setTimeout(function () {
+        preloader.classList.replace("preloader--visible", "preloader--invisible");
+        appWindow.classList.replace("invisible", "visible");
+        pages[0].classList.replace("invisible", "visible");
+        pages[0].classList.replace("above", "center");
+        setTimeout(function () {
+            container[0].classList.remove("delay");
+            preloader.style.display = "none";
+        }, 2000);
+    }, 2500);
 }
 
 function decrement() {
@@ -37,6 +51,7 @@ function decrement() {
         pages[read].classList.replace("center", "below");
         read -= 1;
         let fadeItem = pages[read].querySelector(".container");
+        askForContent();
         if (fadeItem === null) {
             //do nothing
         } else {
@@ -44,16 +59,12 @@ function decrement() {
         }
 
         let preFadeItem = pages[read + 1].querySelector(".container");
-
         if (preFadeItem === null) {
             //do nothing
         } else {
             preFadeItem.classList.replace("visible", "invisible");
         }
     }
-    //if (read < 1) {
-    //    up.classList.replace("visible", "invisible");
-    //}
 }
 
 function subIncrement() {
@@ -76,6 +87,7 @@ function increment() {
     if (read < pages.length - 1) {
         subIncrement();
         let fadeItem = pages[read].querySelector(".container");
+        askForContent();
         if (fadeItem === null) {
             //do nothing
         } else {
@@ -83,30 +95,37 @@ function increment() {
             fadeItem.classList.replace("invisible", "visible");
         }
     }
-    //if (read > 0) {
-    //    up.classList.replace("invisible", "visible");
-    //}
+    if (read === 21) {
+        if (videoLoaded === false) {
+            requestVideo();
+            videoLoaded = true;
+        }
+        else {
+            console.log("video already loaded");
+        }
+    }
 }
 
-window.addEventListener("keydown", function(keydown) {
+window.addEventListener("keydown", function (keydown) {
     let key;
     key = keydown.keyCode;
     if (enabled === true && paused === true) {
         switch (key) {
-            case 38: //Ülesse
+            case 38:
                 decrement();
                 break;
-            case 40: //Alla
+            case 40:
                 increment();
                 break;
         }
 
         console.log("current page: " + read);
         paused = false;
-        setTimeout(function() { paused = true; }, 500);
+        setTimeout(function () { paused = true; }, 500);
     }
 });
-window.addEventListener("wheel", function(wheel) {
+
+window.addEventListener("wheel", function (wheel) {
     let scroll;
     scroll = wheel.deltaY;
     if (enabled === true && paused === true) {
@@ -118,89 +137,104 @@ window.addEventListener("wheel", function(wheel) {
 
         console.log("current page: " + read);
         paused = false;
-        setTimeout(function() { paused = true; }, 500);//navigeerimise kiirus, norm ~1000
+        setTimeout(function () { paused = true; }, 500);
     }
 });
 
-
-const aside = document.querySelector("aside");
-const lisainfo = document.querySelector(".lisaInfo");
-let sisu = document.querySelectorAll(".sisu");
-let current;
-
-let tekst = document.querySelector(".tekst");
-let getInfo = new XMLHttpRequest();
-
-function request(input) {
-    getInfo.onreadystatechange = function() {
-        if (this.readystate == 4 || this.status == 200) {
-            tekst.innerHTML = getInfo.responseText; //viskab teksti sisse
-            console.log("jee"); //kontroll, EEMALDADA
+function askForContent() {
+    console.log(read);
+    let ARV = read - 1;
+    if (isContentLoaded[read] === false) {
+        switch (read) {
+            case 0:
+                console.log("Nothing to load");
+                break;
+            case 21:
+                console.log("Nothing to load");
+                break;
+            default:
+                let contentURL = "text/" + language + "/" + read + ".txt";
+                let fetchContent = new XMLHttpRequest();
+                fetchContent.open("GET", contentURL);
+                fetchContent.overrideMimeType('text/xml; charset=iso-8859-1');
+                fetchContent.send();
+                fetchContent.onreadystatechange = function () {
+                    if (this.status === 200) {
+                        let vastus = decodeURIComponent(fetchContent.response);
+                        container[ARV].innerHTML = vastus; //viskab teksti sisse
+                        console.log("container[ARV] " + container[ARV]);
+                    }
+                };
+                console.log(contentURL);
+                isContentLoaded[read] = true;
         }
-    };
-    getInfo.open("GET", "text/" + input + ".txt");
-    getInfo.send();
-    console.log("jeej");
-
-}
-
-
-
-function peidaSisu() {
-    for (let n = 0; n < sisu.length; n++)
-        sisu[n].classList.replace("visible", "invisible");
-}
-
-function info(input) {
-    peidaSisu();
-    request(input);
-
-    switch (input) {
-        case 13:
-
-            break;
-        default:
-            input = 1;
-            break;
-    };
-
-    current = input;
-    enabled = false;
-    aside.classList.replace("nodisplay", "display");
-    lisainfo.classList.replace("nodisplay", "display");
-    sisu[current].classList.replace("nodisplay", "display");
-    setTimeout(display, 30);
-}
-
-function peida() {
-    lisainfo.classList.replace("visible", "invisible");
-    aside.classList.replace("asideOut", "asideDefault");
-    container[read].removeEventListener("mousedown", peida);
-    enabled = true;
-    peidaSisu();
-    container[read].classList.replace("invisible", "visible");
-    setTimeout(nodisplay, 3000);
-}
-
-function display() {
-    container[read].classList.replace("visible", "invisible");
-    lisainfo.classList.replace("invisible", "visible");
-    aside.classList.replace("asideDefault", "asideOut");
-
-    sisu[current].classList.replace("invisible", "visible");
-}
-
-function nodisplay() {
-    if (enabled === true) {
-        lisainfo.classList.replace("display", "nodisplay");
-        aside.classList.replace("display", "nodisplay");
-        for (let n = 0; n < sisu.length; n++)
-            sisu[n].classList.replace("display", "nodisplay");
+    }
+    else {
+        console.log("Content already loaded.");
     }
 }
 
-function tagasiÜlesse() {
-    for (read; read > 0;) {
-        decrement();
+function resetContentStates() {
+    for (var stateIndex = 0; stateIndex < pageCount; stateIndex++) {
+        isContentLoaded[stateIndex] = false;
+        console.log(isContentLoaded[stateIndex]); //State given;
+    }
+    console.log(isContentLoaded.length); //No of loadable content;
+}
+
+function requestVideo() {
+    let videoDiv = document.querySelector(".js-video");
+    console.log(videoDiv);
+    videoDiv.innerHTML = '<video width="720" height="404" controls><source src="media/video.mp4" type="video/mp4"></video>';
+}
+function requestArchiveDoc() {
+    let archiveDiv = document.querySelector(".js-archive");
+    archiveDiv.innerHTML = '<embed src="media/arhiiv.pdf" type="application/pdf" width="100%" height="100%" />';
+}
+
+//function tagasiÜlesse() {
+//    for (read; read > 0;) {
+//        decrement();
+//    }
+//}
+
+function changeLanguage(langSwitch) {
+    if (langSwitch === language) {
+        console.log("Nothing to change");
+    }
+    else {
+        language = langSwitch;
+        console.log("changed to " + langSwitch);
+        resetContentStates();
+
+    }
+
+}
+
+function reloadCurrentPageContent() {
+    console.log(read);
+    let ARV = read - 1;
+    switch (read) {
+        case 0:
+            console.log("Nothing to load");
+            break;
+        case 21:
+            console.log("Nothing to load");
+            break;
+        default:
+            let contentURL = "text/" + language + "/" + read + ".txt";
+            let fetchContent = new XMLHttpRequest();
+            fetchContent.open("GET", contentURL);
+            fetchContent.overrideMimeType('text/xml; charset=iso-8859-1');
+            fetchContent.send();
+            fetchContent.onreadystatechange = function () {
+                if (this.status === 200) {
+                    let vastus = decodeURIComponent(fetchContent.response);
+                    container[ARV].innerHTML = vastus; //viskab teksti sisse
+                    console.log("container[ARV] " + container[ARV]);
+                }
+            };
+            console.log(contentURL);
+            isContentLoaded[read] = true;
     }
 }
